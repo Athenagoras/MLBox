f984d5948d43b249b7613012b1a529a4c4f78b07
Axel <axelderomblay@gmail.com>
2017-07-20 17:04:41 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -86,7 +86,7 @@ source_suffix = '.rst'
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'readme'
+master_doc = 'index'
 
 # General information about the project.
 project = u'MLBox'
